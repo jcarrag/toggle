8f6e9b619903c54dace858e8d82f9659911ed624
James Carragher <james@carragher.org>
2022-04-27 10:14:05 +0100

    replace with toggleable button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  // "initial" is used to prevent the toggle-button CSS from matching & running on page load
+  const [togglePosition, setTogglePosition] = React.useState<
+    "initial" | "left" | "right"
+  >(() => "initial");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw" }}>
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          height: 100,
+        }}
+      >
+        <div
+          className="toggle-button"
+          {...{ togglePosition }}
+          style={{
+            position: "absolute",
+            height: 100,
+            width: "50%",
+            left: 0,
+            backgroundColor: "red",
+            opacity: 0.5,
+            borderRadius: 50,
+          }}
+          onClick={() =>
+            setTogglePosition(
+              togglePosition === "left" || togglePosition === "initial"
+                ? "right"
+                : "left"
+            )
+          }
+        ></div>
+        <p>Left text</p>
+        <p>Right text</p>
+      </div>
     </div>
   );
 }
